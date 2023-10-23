486b3c8a58e559acd7dbfdfcc268c804b615dd64
onarg <onargo@gmail.com>
2023-10-23 15:43:46 -0700

    Finished password verification and added shake, while typing cyrillic letters. Also moved show password button to the left of input box

--- js/registerScript.js
+++ js/registerScript.js
@@ -1,101 +1,148 @@
 let date
-const UPPER_CASE_REGEX = /[A-Z]/;
-const SPECIAL_CHAR_REGEX = /[!@#$%^&*(),.?":{}|<>]/;
-const DIGIT_REGEX = /[0-9]/;
+let passwordErrorMessages = [];
+let tempError = true
+const REGEX = {
+    UPPER_CASE: /[A-Z]/,
+    SPECIAL_CHAR: /[!@#$%^&*(),.?":{}|<>]/,
+    DIGIT: /[0-9]/,
+    EMAIL: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+    NAME_UK:  /[А-Яа-яҐґЄєІіЇї`']+/gu,
+    CYRILLIC: /[А-Яа-яҐґЄєІіЇїЁё`']+/gu,
+};
+
+const ERROR_MESSAGES = {
+    UPPER_CASE: 'Password must contain at least one uppercase letter',
+    SPECIAL_CHAR: 'Password must contain at least one special character',
+    DIGIT: 'Password must contain at least one digit',
+    DATE: 'Password cannot contain your birth date',
+    NAME: 'You cannot have your name in password',
+    LENGTH_SHORT: 'Password must be at least 8 characters',
+    LENGTH_LONG: 'Password cannot be longer than 24 characters',
+    EMAIL_INVALID: 'Некоректна email адреса. Будь-ласка перевірте, чи ви вказали її правильно',
+    NAME_INVALID: 'Це поле має бути заповнено лише українською мовою',
+    CYRILLIC: 'You cannot enter cyrillic symbols in this field'
+};
+
+const hasUpperCase = (str) => REGEX.UPPER_CASE.test(str);
+const hasSpecialChar = (str) => REGEX.SPECIAL_CHAR.test(str);
+const hasDigit = (str) => REGEX.DIGIT.test(str);
 
 
 function showError(elementId, message) {
-    const element = document.getElementById(elementId+'ErrorMessage')
+    const element = document.getElementById(elementId+'PermanentErrorMessage')
     element.classList.toggle('errorText', !!message);
     element.innerText = message.toString() || '';
     element.style.display = ''
 }
+
+
+function showTemporaryError(elementId, message, duration) {
+    const element = document.getElementById(elementId + 'TemporaryErrorMessage');
+    if (element.querySelector('.temporaryError')) {
+        return;
+    }
+    const tempErrorDiv = document.createElement('div');
+    tempErrorDiv.className = 'temporaryError';
+    tempErrorDiv.innerText = message;
+    tempErrorDiv.style.color = 'red';
+    element.appendChild(tempErrorDiv);
+
+    setTimeout(() => {
+        if (document.body.contains(tempErrorDiv)) {
+            element.removeChild(tempErrorDiv);
+        }
+    }, duration);
+}
+
 function nameValidator(name) {
-    const regCheck = /[А-Яа-яҐґЄєІіЇї`']+/gu
-     if(regCheck.test(name.value) || name.value.length===0) {
-         showError(name.id, '')
-         return true
-     }
-    else {
-        showError(name.id, 'Це поле має бути заповнено лише українською мовою')
-         return false
-     }
-}
-function emailValidator(email){
-    let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    if(regex.test(email.value)){
-        showError(email.id, '')
-        return true
-    }
-    else if(email.value.length === 0){
-        showError(email.id, '')
-        return false
-    }
-    showError(email.id, 'Некоректна email адреса. Будь-ласка перевірте, чи ви вказали її правильно')
-    return false
-}
-function hasUpperCase(str) {
-    return UPPER_CASE_REGEX.test(str);
+    const isValid = REGEX.NAME_UK.test(name.value) || name.value.length === 0;
+    showError(name.id, isValid ? '' : ERROR_MESSAGES.NAME_INVALID);
+    return isValid;
 }
 
-function hasSpecialChar(str) {
-    return SPECIAL_CHAR_REGEX.test(str);
+function emailValidator(email) {
+    const isValid = REGEX.EMAIL.test(email.value) || email.value.length === 0;
+    showError(email.id, isValid ? '' : ERROR_MESSAGES.EMAIL_INVALID);
+    return isValid;
 }
 
-function hasDigit(str) {
-    return DIGIT_REGEX.test(str);
-}
 function hasDate(str, date) {
     const datePatterns = (date.toString() + '-' + date.toString().slice(2,4)).split('-');
-    return datePatterns.some(pattern => str.includes(pattern));
+    return atLeastTwo(datePatterns, str)
 }
-function passwordValidator(passwordInput) {
-    const password = passwordInput.value;
-
-
-    if (password.length === 0) {
-        showError(passwordInput.id, '');
-        return false;
-    }
-
-    const errorMessages = [];
-
-    if (!hasUpperCase(password)) {
-        errorMessages.push('Password must contain at least one uppercase letter\n');
-    }
-    if (!hasSpecialChar(password)) {
-        errorMessages.push('Password must contain at least one special character\n');
-    }
-    if (!hasDigit(password)) {
-        errorMessages.push('Password must contain at least one digit\n');
-    }
-    // Assuming `date` is defined elsewhere in your code
-    if (hasDate(password, date)) {
-        errorMessages.push('Password cannot contain the selected date\n');
-    }
-    if (password.length < 8) {
-        errorMessages.push('Password must be at least 8 characters\n');
-    }
-    if (password.length > 24) {
-        errorMessages.push('Password cannot be longer than 24 characters\n');
-    }
-
-    showError(passwordInput.id, errorMessages.join(''));
-    return errorMessages.length === 0;
-}
-
-function checkString(givenString, stringList) {
-    let count = 0;
-    for (let i = 0; i < stringList.length; i++) {
-        const stringElement = String(stringList[i]);
-        if (givenString.includes(stringElement)) {
-            count++;
-            if (count >= 2) {
-                return true;
-            }
+function atLeastTwo(array, str){
+    let refArray = array
+    let counter = 0
+    for(let i = 0;i<refArray.length;i++){
+        if(str.includes(refArray[i])) {
+            counter++
+            refArray.splice(i,1)
+            i=0
         }
     }
-    return false;
+    return counter>1
+}
+
+function nameFilled(){
+    let idList = []
+    if(document.getElementById('name').value.length>0) idList.push('name')
+    if(document.getElementById('surname').value.length>0) idList.push('surname')
+    if(document.getElementById('midname').value.length>0) idList.push('midname')
+    return idList
+}
+function containName(str){
+    const names = nameFilled().filter((id) =>{
+        return str.includes(document.getElementById(id).value);
+    })
+    return names.length>0
+}
+
+function shakePasswordContainer() {
+    const container = document.getElementById('password-input');
+    container.classList.add('shake');
+    container.addEventListener('animationend', () => {
+        container.classList.remove('shake');
+    });
+}
+
+const isCyrillic = (str) => REGEX.CYRILLIC.test(str)
+function passwordValidator(passwordInput) {
+    passwordErrorMessages = []
+
+    const password = passwordInput.value;
+
+    if (password.length === 0) {
+        return false;
+    }
+    if(isCyrillic(password)) {
+        showTemporaryError(passwordInput.id, ERROR_MESSAGES.CYRILLIC, 4000);
+        shakePasswordContainer();
+        passwordInput.value = password.replace(REGEX.CYRILLIC, '')
+    }
+    if (!hasUpperCase(password)) {
+        passwordErrorMessages.push(ERROR_MESSAGES.UPPER_CASE);
+    }
+    if (!hasSpecialChar(password)) {
+        passwordErrorMessages.push(ERROR_MESSAGES.SPECIAL_CHAR);
+    }
+    if (!hasDigit(password)) {
+        passwordErrorMessages.push(ERROR_MESSAGES.DIGIT);
+    }
+    if (hasDate(password, date)) {
+        passwordErrorMessages.push(ERROR_MESSAGES.DATE);
+    }
+    if (containName(password)) {
+        passwordErrorMessages.push(ERROR_MESSAGES.NAME);
+    }
+    if (password.length < 8) {
+        passwordErrorMessages.push(ERROR_MESSAGES.LENGTH_SHORT);
+    }
+    if (password.length > 24) {
+        passwordErrorMessages.push(ERROR_MESSAGES.LENGTH_LONG);
+    }
+
+    showError(passwordInput.id, passwordErrorMessages.join('\n'));
+    return passwordErrorMessages.length === 0;
 }
 
 function checkAll(){
@@ -156,7 +203,7 @@ function setYesterdayDate(){
     dateInput.setAttribute('max', yesterdayDate);
     dateInput.setAttribute('min', minDate)
     dateInput.value = yesterdayDate;
-    date = document.getElementById('date').value
+    return yesterdayDate
 }
 
 function toggleCheckboxes() {
@@ -192,26 +239,23 @@ function duplicateRows() {
     }
 }
 
-
-
-document.getElementById('showPassword').addEventListener('mousedown', function() {
+function showPassword() {
     document.getElementById('password').type = 'text';
-});
+}
 
-document.getElementById('showPassword').addEventListener('mouseup', function() {
+function hidePassword() {
     document.getElementById('password').type = 'password';
-});
+}
 
-document.getElementById('showPassword').addEventListener('mouseleave', function() {
-    document.getElementById('password').type = 'password';
-});
+function updateDate() {
+    date = document.getElementById('date').value;
+}
+function clearErrorMessages(){
+    const password = document.getElementById('password').value
+    if(password.length===0) showError('password', '')
+}
 
-document.addEventListener('DOMContentLoaded', function() {
-    setYesterdayDate()
-});
-
-
-document.getElementById('telephone').addEventListener('input', function(e) {
+function formatTelephone(e) {
     let value = e.target.value.replace(/\D/g, '');
     let formatted = '+38(0';
     for (let i = 0; i < value.length; i++) {
@@ -220,6 +264,17 @@ document.getElementById('telephone').addEventListener('input', function(e) {
         else if (i === 7) formatted += '-';
         formatted += value[i];
     }
+}
 
-    e.target.value = formatted;
+function Listeners() {
+    document.getElementById('password').addEventListener("input", clearErrorMessages)
+    document.getElementById('showPassword').addEventListener('mousedown', showPassword);
+    document.getElementById('showPassword').addEventListener('mouseup', hidePassword);
+    document.getElementById('showPassword').addEventListener('mouseleave', hidePassword);
+    document.getElementById('telephone').addEventListener('input', formatTelephone);
+    document.getElementById('date').addEventListener('input', updateDate);
+}
+document.addEventListener('DOMContentLoaded', () => {
+    date = setYesterdayDate();
+    Listeners();
 });
